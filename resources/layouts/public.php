c56fc8c47acf161b06e7d8dc97d5bfa591851dd0
Jason Vance <jason@namesonic.com>
2023-12-07 18:55:49 -0800

    Slap in some W3.CSS styling for good measure

--- resources/layouts/public.php
+++ resources/layouts/public.php
@@ -1,19 +1,14 @@
 <html>
 <head>
     <title>{{ title }}</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-    <div>
-        <ul>
-            <li>
-                <a href="?p=home">Home</a>
-            </li>
-            <li>
-                <a href="?p=about">About</a>
-            </li>
-        </ul>
-    </div>
-    <hr>
-    {{ body }}
+<div class="w3-bar w3-black">
+    <a href="?p=home" class="w3-bar-item w3-button">Home</a>
+    <a href="?p=about" class="w3-bar-item w3-button">About</a>
+    <a href="?p=contact" class="w3-bar-item w3-button">Contact</a>
+</div>
+{{ body }}
 </body>
 </html>
--- resources/views/home.php
+++ resources/views/home.php
@@ -1,27 +1,30 @@
-<h1>
-    Simple Site Example
-</h1>
+<div class="w3-container">
+    <h1>
+        Simple Site Example
+    </h1>
 
-<h2>
-    Lightweight HTML Template Framework
-</h2>
+    <h2>
+        Lightweight HTML Template Framework
+    </h2>
 
-<p>
-    Requires
-</p>
+    <p>
+        Requires
+    </p>
 
-<ul>
-    <li>PHP</li>
-</ul>
+    <ul>
+        <li>PHP</li>
+        <li>W3.CSS</li>
+    </ul>
 
-<h3>Installation</h3>
+    <h3>Installation</h3>
 
-<p>Clone the repo into a new folder since it may overwrite your existing files.</p>
+    <p>Clone the repo into a new folder since it may overwrite your existing files.</p>
 
-<p>
-    A webserver is required to process the .htaccess file which will properly map the page URL to the PHP script.  Otherwise you can use ?p=pagename as supplied in the examples.
-</p>
+    <p>
+        A webserver is required to process the .htaccess file which will properly map the page URL to the PHP script.  Otherwise you can use ?p=pagename as supplied in the examples.
+    </p>
 
-<p>
-    Here is an example of a link to a page that has not yet been prepared <a href="?p=contact">Contact Us Link</a>.  Since the URL address is "?p=contact" or "/contact" you will need to create an App\Controller\ContactController.php class to handle the page.
-</p>
+    <p>
+        Here is an example of a link to a page that has not yet been prepared <a href="?p=contact">Contact Us Link</a>.  Since the URL address is "?p=contact" or "/contact" you will need to create an App\Controller\ContactController.php class to handle the page.
+    </p>
+</div>
