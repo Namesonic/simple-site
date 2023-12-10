d392c1d2dce5fe1f912afff0bd67243583f6992f
Jason Vance <jason@namesonic.com>
2023-12-10 12:55:48 -0800

    Removes old framework code / adds framework via composer

--- src/Http/Controllers/Controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimpleSite\Http\Controllers;
-
-class Controller {
-    public function __construct() {}
-}
--- src/bootstrap/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//
-//use SimpleSite\Http\Exceptions\Handler;
-//
-//$handler = new Handler();
-//set_exception_handler([$handler, 'handle']);
-
